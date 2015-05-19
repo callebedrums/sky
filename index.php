4350966a62337c43c943565e26c27fff2a08ddd5
Callebe Gomes <callebedrums@gmail.com>
2015-05-19 11:44:07 -0300

    adding some stuffs

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Hellow World!</title>
+	<link rel="stylesheet" type="text/css" href="/static/css/sky.css" />
+</head>
+<body>
+	<h1><?php echo "Hellow World!"; ?></h1>
+	<img src="/static/img/cloud.png" alt="cloud" />
+	
+	<p><?php var_dump($_GET); ?></p>
+	
+	<script type="text/javascript" src="/static/js/sky.js"></script>
+</body>
+</html>
